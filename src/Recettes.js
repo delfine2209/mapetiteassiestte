7cf221cfd520a21e1a61b17a7bb4ff9c1b9a59c8
delfine2209 <delphine.schaller@orange.fr>
2026-09-24 08:32:02 +0200

    Add fetching and displaying recettes from JSON
    Fetch recette data from recettes.json and display in cards.

--- src/Recettes.js
+++ src/Recettes.js
@@ -1,1 +1,29 @@
+fetch("recettes.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const container =
+      document.getElementById("recettes");
+
+    data.forEach(recette => {
+
+      container.innerHTML += `
+        <div class="card">
+
+          <h3>${recette.nom}</h3>
+
+          <p>
+            ⏱ ${recette.temps} min
+          </p>
+
+          <p>
+            🥩 ${recette.proteines} g protéines
+          </p>
+
+        </div>
+      `;
+
+    });
+
+  });
 
